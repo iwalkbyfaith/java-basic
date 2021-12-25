022b118b859fc63f65fd2dee3d36b5cb644fa9e2
iwalkbyfaith <godownthestreet@naver.com>
2021-12-25 23:19:57 +0900

    21.12.25 update

---
+++ 20211224/src/practice/Practice01.java
@@ -0,0 +1,35 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Practice01 {
+	public static void main(String[] args) {
+		
+		
+		// 스캐너로 받은 문장을 .charAt() 과 .length로 세로 출력
+		
+		// .charAt()은 한 단어씩 받아오는 애고
+		// .length는 길이 재는 애
+		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("문장을 입력해주세요.");
+		String a = scan.nextLine();
+				
+		
+		for(int i = 0; i < a.length(); i++) {
+			System.out.println(a.charAt(i));
+		}
+
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
